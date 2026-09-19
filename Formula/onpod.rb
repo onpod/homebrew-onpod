f03f396a90629997603e2aeb58412973f5ecfb6f
latemonk <63733683+latemonk@users.noreply.github.com>
2026-09-19 12:50:39 +0900

    onpod 0.7.118

--- Formula/onpod.rb
+++ Formula/onpod.rb
@@ -15,22 +15,22 @@ class Onpod < Formula
   on_macos do
     on_arm do
       url "https://onpod.ai/cli/onpod-darwin-arm64?v=0.7.118"
-      sha256 "280140004291207cc377793c1ff44ac3a686f838c77c36e61e82893b7a613b67"
+      sha256 "d4c10087748b75c9d74f8de229dbb485bcfd0b2e02964c82e5863e265d588962"
     end
     on_intel do
       url "https://onpod.ai/cli/onpod-darwin-amd64?v=0.7.118"
-      sha256 "b4db4a29d2a88a076cdde1dffca826f6baef1d055d8ca16fad9b66c140a97c78"
+      sha256 "10b6ae3119610a38dd996468557e07420e72d2dbd9c87c0be5845f574d06c4f7"
     end
   end
 
   on_linux do
     on_arm do
       url "https://onpod.ai/cli/onpod-linux-arm64?v=0.7.118"
-      sha256 "83677e3e4456520b14b44264a0e1fb0fe1d5918bcab335ea266c759b13e4d102"
+      sha256 "aa3f2dd4d4cf5e35388ef271c35264fe98791ee841e0a42b8bb982e3dac85c2c"
     end
     on_intel do
       url "https://onpod.ai/cli/onpod-linux-amd64?v=0.7.118"
-      sha256 "700c599d54ec515eb3f5ef998adcdd6f9f0ec2cbbd2ddc1b9a22523451ed4b9b"
+      sha256 "dc89aa070e65e43a1639b0c27935bc445736d8e66f5e0c7e58ac21789e89996b"
     end
   end
 
